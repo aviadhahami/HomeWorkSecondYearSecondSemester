c6cd458b39ce43b782fa6aa2d934d4045fd6925e
Aviad Hahami <aviadhahami@gmail.com>
2015-03-03 11:33:12 +0200

    Fixed grayscale
    still stuck on the X Deriv

--- Image Proccessing/first excersize/CG_ex1_partial/src/edu/cg/ImageProc.java
+++ Image Proccessing/first excersize/CG_ex1_partial/src/edu/cg/ImageProc.java
@@ -40,63 +40,37 @@ public class ImageProc {
 	}
 
 	public static BufferedImage horizontalDerivative(BufferedImage img) {
-		BufferedImage out = new BufferedImage(img.getWidth(), img.getHeight(),
-				img.getType());
-		try {
+		BufferedImage grayed = grayScale(img);
+		BufferedImage out = new BufferedImage(grayed.getWidth(),
+				grayed.getHeight(), grayed.getType());
 
-			for (int x = 0; x < img.getWidth(); x++) {
-				for (int y = 0; y < img.getHeight(); y++) {
-					if (x == 0) {
-						out.setRGB(x, y, 0);
-					} else {
-						/*
-						 * Color rgb1 = new Color(img.getRGB(x, y)); int red1 =
-						 * rgb1.getRed(); int green1 = rgb1.getGreen(); int
-						 * blue1 = rgb1.getBlue();
-						 * 
-						 * Color rgb2 = new Color(img.getRGB(x - 1, y)); int
-						 * red2 = rgb2.getRed(); int green2 = rgb2.getGreen();
-						 * int blue2 = rgb2.getBlue();
-						 * 
-						 * int redAvg = ((red1 - red2) + 255) / 2; int greenAvg
-						 * = ((green1 - green2) + 255) / 2; int blueAvg =
-						 * ((blue1 - blue2) + 255) / 2;
-						 * 
-						 * // int sum = redAvg + greenAvg + blueAvg; Color
-						 * xDerColor = new Color(redAvg, greenAvg, blueAvg);
-						 */
-						int I = img.getRGB(x, y);
-						int I_1 = img.getRGB(x, y);
+		for (int x = 0; x < grayed.getWidth(); x++) {
+			for (int y = 0; y < grayed.getHeight(); y++) {
+				if (x == grayed.getWidth() - 1) {
+					out.setRGB(x, y, 0);
+				} else {
+					int I = img.getRGB(x + 1, y);
+					int I_1 = img.getRGB(x, y);
 
-						out.setRGB(x, y, I - I_1);
-					}
+					out.setRGB(x, y, I - I_1);
 				}
 			}
-			return out;
-		} catch (Exception e) {
-			System.out.println(e);
-			return null;
 		}
+		return grayed;
 	}
 
 	public static BufferedImage verticalDerivative(BufferedImage img) {
 		BufferedImage out = new BufferedImage(img.getWidth(), img.getHeight(),
 				img.getType());
-		int currentDerivitive;
-		for (int x = 0; x < img.getWidth(); x++) {
-			for (int y = 0; y < img.getHeight(); y++) {
 
-				currentDerivitive = img.getRGB(x, y + 1) - img.getRGB(x, y - 1);
-
-				out.setRGB(x, y, currentDerivitive);
-			}
-		}
 		return out;
 	}
 
 	public static BufferedImage gradientMagnitude(BufferedImage img) {
-		// TODO implement this
-		return null;
+		BufferedImage out = new BufferedImage(img.getWidth(), img.getHeight(),
+				img.getType());
+		// implement
+		return out;
 	}
 
 	public static BufferedImage retargetSize(BufferedImage img, int width,
