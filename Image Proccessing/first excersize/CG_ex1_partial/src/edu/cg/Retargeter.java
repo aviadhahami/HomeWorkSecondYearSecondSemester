4695ec55c1aff93731557a741f15c9f26e86f20d
Aviad Hahami <aviadhahami@gmail.com>
2015-03-08 23:32:11 +0200

    done with calculateCostsMatrix() :+1:

--- Image Proccessing/first excersize/CG_ex1_partial/src/edu/cg/Retargeter.java
+++ Image Proccessing/first excersize/CG_ex1_partial/src/edu/cg/Retargeter.java
@@ -17,15 +17,14 @@ public class Retargeter {
 		// of the seam order matrix. You should implement this.
 		this.originalImg = m_img;
 		this.currImg = m_img;
-		this.seamOrderMatrix = calculateSeamsOrderMatrix();
 	}
 
 	public int[][] getSeamsOrderMatrix() {
-		return this.calculateSeamsOrderMatrix();
+		return this.seamOrderMatrix;
 	}
 
-	public int[] getOrigPosMatrix() {
-		return this.calculateCostsMatrix(this.currImg.getWidth());
+	public BufferedImage getOrigPosMatrix() {
+		return this.originalImg;
 	}
 
 	public BufferedImage retarget(int newSize) {
@@ -33,28 +32,51 @@ public class Retargeter {
 		return null;
 	}
 
-	private int[][] calculateSeamsOrderMatrix() {
+	private void calculateSeamsOrderMatrix() {
 
+			calculateCostsMatrix();
 		// TODO implement this - this calculates the order in which seams are
 		// extracted
-		return null;
 
 	}
 
-	private int[][] calculateCostsMatrix(int w) {
+	private void calculateCostsMatrix() {
+		// Using forward energy
 
 		// TODO implement this - cost matrix should be calculated for a given
 		// image width w
 		// to be used inside calculateSeamsOrderMatrix()
-		int[][] costMatrix = new int[this.currImg.getWidth()][this.currImg.getHeight()];
+
+		BufferedImage intensityImg = ImageProc.grayScale(this.currImg);
+		BufferedImage gradMagMatrix = ImageProc.gradientMagnitude(this.currImg);
+
+		// moving for easier access
+		int[][] costMatrix = new int[gradMagMatrix.getWidth()][gradMagMatrix.getHeight()];
+		for (int i = 0; i < gradMagMatrix.getWidth(); i++) {
+			for (int j = 0; j < gradMagMatrix.getHeight(); j++) {
+				costMatrix[i][j] = gradMagMatrix.getRGB(i, j);
+			}
+
+		}
+
 		// TODO: Flow - we should calc this matrix using the base value of the
 		// gradient
 		// plus the "price"/cost of the pixel's value in the grayscale mode
+
 		for (int x = 0; x < this.currImg.getWidth(); x++) {
 			for (int y = 0; y < this.currImg.getHeight(); y++) {
 				// TODO: create edge case for edges
 
 				// init the costs
+				int costLeft = Math.abs(intensityImg.getRGB(x, y + 1) - intensityImg.getRGB(x, y - 1));
+				costLeft += Math.abs(intensityImg.getRGB(x - 1, y) - intensityImg.getRGB(x, y - 1));
+
+				int costVertical = Math.abs(intensityImg.getRGB(x, y + 1) - intensityImg.getRGB(x, y - 1));
+
+				int costRight = Math.abs(intensityImg.getRGB(x, y + 1) - intensityImg.getRGB(x, y - 1));
+				costRight += Math.abs(intensityImg.getRGB(x - 1, y) - intensityImg.getRGB(x, y - 1));
+
+				// finalize the summation
 				int totalCostLeft = costMatrix[x - 1][y - 1] + costLeft;
 				int totalCostVertical = costMatrix[x - 1][y] + costVertical;
 				int totalCostRight = costMatrix[x - 1][y - 1] + costRight;
@@ -65,7 +87,7 @@ public class Retargeter {
 			}
 		}
 
-		return costMatrix;
+		this.costsMatrix = costMatrix;
 	}
 
 }
