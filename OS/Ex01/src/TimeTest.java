5cb69cdd68f5138200c432a2a338dbf7d5a62c6a
Aviad Hahami <aviadhahami@gmail.com>
2015-03-09 18:13:07 +0200

    Started OS homework :clock1:

---
+++ OS/Ex01/src/TimeTest.java
@@ -0,0 +1,21 @@
+/*
+ * @Author - Aviad Hahami
+ */
+public class TimeTest {
+
+	public static void main(String args, String sPath, String tPath, int buffSize) {
+
+		boolean forceFlag = false; //will not overwrite
+		
+
+		/*
+		 * The application should receive the source file path, target file path
+		 * and buffer size as command line arguments. By default, the
+		 * application does not overwrite an existing file. This can be forced
+		 * by adding the /force argument.
+		 */
+		//Usage: java TimeTest [/force] source_file target_file buffer_size
+
+	}
+
+}
