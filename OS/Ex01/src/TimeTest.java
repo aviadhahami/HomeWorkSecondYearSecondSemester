73bd1567ec0e5f6739fe73df28457ffa024a1b17
Aviad Hahami <aviadhahami@gmail.com>
2015-03-10 11:10:43 +0200

    we go we go

--- OS/Ex01/src/TimeTest.java
+++ OS/Ex01/src/TimeTest.java
@@ -2,25 +2,17 @@
  * @Author - Aviad Hahami
  */
 public class TimeTest {
-	// force-flagged main method
+
 	public static void main(String[] args) {
 
-		/*
-		 * The application should receive the source file path, target file path
-		 * and buffer size as command line arguments. By default, the
-		 * application does not overwrite an existing file. This can be forced
-		 * by adding the /force argument.
-		 */
-		// Usage: java TimeTest [/force] source_file target_file buffer_size
-
-		//not enough args -> we're out
+		// not enough args -> we're out
 		if (args[3] == null) {
 			System.err.println("check inputs");
 			System.exit(1);
 		}
 
-		String sourceFilePath, destFilePath;
-		int buffSize;
+		String sourceFilePath = null, destFilePath = null;
+		int buffSize = 0;
 		// some input checks
 		boolean forceFlag = args[0] == "/force" ? true : false;
 		try {
@@ -42,6 +34,45 @@ public class TimeTest {
 		}
 
 		// done playing with inputs. GOGO!
+
+		// copy file
+		long startTime = System.currentTimeMillis();
+		// copy logic
+		boolean result = copyFile(sourceFilePath, destFilePath, buffSize, forceFlag);
+		if (!result) {
+			// copy didnt go well -> we leave
+			System.err.println("Copying failed.");
+			System.exit(1);
+		}
+		long endTime = System.currentTimeMillis();
+
+		System.out.println("File " + sourceFilePath + " was copied to " + destFilePath);
+		System.out.println("Runtime: " + (endTime - startTime));
+
+		/*
+		 * Guidelines for the force flag: If the force flag was present, the
+		 * copy operation should overwrite the target file, if not, and a file
+		 * exists, some error message should be displayed and the copy will not
+		 * overwrite the existing file
+		 */
+	}
+
+	/*
+	 * Copies a file to a specific path, using the specified buffer size.
+	 * 
+	 * @param srcFileName File to copy
+	 * 
+	 * @param toFileName Destination file name
+	 * 
+	 * @param bufferSize Buffer size in bytes
+	 * 
+	 * @param bOverwrite If file already exists, overwrite it
+	 * 
+	 * @return true when copy succeeds, false otherwise
+	 */
+
+	public static boolean copyFile(String srcFileName, String toFileName, int bufferSize, boolean bOverwrite) {
+		return bOverwrite;
 
 	}
 }
