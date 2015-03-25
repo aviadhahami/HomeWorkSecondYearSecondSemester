389364142fb88bd79f1b0010012315465266fb35
Aviad Hahami <aviadhahami@gmail.com>
2015-03-25 23:31:24 +0200

    Started implementing Insert

--- DS/Ex03 - Heap/src/Heap.java
+++ DS/Ex03 - Heap/src/Heap.java
@@ -1,20 +1,39 @@
 public class Heap {
 
 	private Node root;
+	private int heapSize;
 
 	public Heap() {
 		this.setRoot(null);
+		this.heapSize = 0;
 	}
 
 	public Heap(Node root) {
 		this.setRoot(root);
+		this.heapSize = 1;
 	}
+
+	public void insert(Node node){
+		this.heapSize++;
+		int size = this.heapSize -1 ;
+		while (i > 0) {
+			type type = (type) en.nextElement();
+			
+		}
+	}
+	
+	/* GETTERS AND SETTERS BELOW ! */
 
 	public Node getRoot() {
 		return root;
 	}
 
-	public void setRoot(Node root) {
+	private void setRoot(Node root) {
 		this.root = root;
 	}
+
+	public int getHeapSize() {
+		return heapSize;
+	}
+
 }
