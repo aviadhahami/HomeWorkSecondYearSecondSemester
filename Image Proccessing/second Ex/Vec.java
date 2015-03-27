df885c61e08b732253c82bb33e0be1e821f84a6b
Aviad Hahami <aviadhahami@gmail.com>
2015-03-27 23:33:28 +0300

    Ex 02 in IP got out

---
+++ Image Proccessing/second Ex/Vec.java
@@ -0,0 +1,350 @@
+package cg.rec.vec;
+
+/**
+ * 3D vector class that contains three doubles. Could be used to represent
+ * Vectors but also Points and Colors.
+ * 
+ */
+public class Vec {
+
+	/**
+	 * Vector data. Allowed to be accessed publicly for performance reasons
+	 */
+	public double x, y, z;
+
+	/**
+	 * Initialize vector to (0,0,0)
+	 */
+	public Vec() {
+		this.x = 0;
+		this.y = 0;
+		this.z = 0;
+	}
+
+	/**
+	 * Initialize vector to given coordinates
+	 * 
+	 * @param x
+	 *            Scalar
+	 * @param y
+	 *            Scalar
+	 * @param z
+	 *            Scalar
+	 */
+	public Vec(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	/**
+	 * Initialize vector values to given vector (copy by value)
+	 * 
+	 * @param v
+	 *            Vector
+	 */
+	public Vec(Vec v) {
+		this.x = v.x;
+		this.y = v.y;
+		this.z = v.z;
+	}
+
+	/**
+	 * Calculates the reflection of the vector in relation to a given surface
+	 * normal. The vector points at the surface and the result points away.
+	 * 
+	 * @return The reflected vector
+	 */
+	public Vec reflect(Vec normal) {
+		Vec finalVec = generateLocalVec();
+		return sub(finalVec, scale(dotProd(finalVec, normal) * 2, normal));
+	}
+
+	private Vec generateLocalVec() {
+		return new Vec(this.x, this.y, this.z);
+	}
+
+	/**
+	 * Adds a to vector
+	 * 
+	 * @param a
+	 *            Vector
+	 */
+	public void add(Vec a) {
+		this.x += a.x;
+		this.y += a.y;
+		this.z += a.z;
+	}
+
+	/**
+	 * Subtracts from vector
+	 * 
+	 * @param a
+	 *            Vector
+	 */
+	public void sub(Vec a) {
+		this.x -= a.x;
+		this.y -= a.y;
+		this.y -= a.z;
+	}
+
+	/**
+	 * Multiplies & Accumulates vector with given vector and a. v := v + s*a
+	 * 
+	 * @param s
+	 *            Scalar
+	 * @param a
+	 *            Vector
+	 */
+	public void mac(double s, Vec a) {
+		this.x += a.x * s;
+		this.y += a.y * s;
+		this.z += a.z * z;
+	}
+
+	/**
+	 * Multiplies vector with scalar. v := s*v
+	 * 
+	 * @param s
+	 *            Scalar
+	 */
+	public void scale(double s) {
+		this.x *= s;
+		this.y *= s;
+		this.z *= s;
+	}
+
+	/**
+	 * Pairwise multiplies with another vector
+	 * 
+	 * @param a
+	 *            Vector
+	 */
+	public void scale(Vec a) {
+		this.x *= a.x;
+		this.y *= a.y;
+		this.z *= a.z;
+	}
+
+	/**
+	 * Inverses vector
+	 * 
+	 * @return Vector
+	 */
+	public void negate() {
+		this.x *= -1;
+		this.y *= -1;
+		this.z *= -1;
+	}
+
+	/**
+	 * Computes the vector's magnitude
+	 * 
+	 * @return Scalar
+	 */
+	public double length() {
+		return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+	}
+
+	/**
+	 * Computes the vector's magnitude squared. Used for performance gain.
+	 * 
+	 * @return Scalar
+	 */
+	public double lengthSquared() {
+		return (this.x * this.x + this.y * this.y + this.z * this.z);
+	}
+
+	/**
+	 * Computes the dot product between two vectors
+	 * 
+	 * @param a
+	 *            Vector
+	 * @return Scalar
+	 */
+	public double dotProd(Vec a) {
+		return (this.x * a.x + this.y * a.y + this.z * a.z);
+	}
+
+	/**
+	 * Normalizes the vector to have length 1. Throws exception if magnitude is
+	 * zero.
+	 * 
+	 * @throws ArithmeticException
+	 */
+	public void normalize() throws ArithmeticException {
+		double vecLength = length();
+		if (vecLength <= 0) {
+			throw new ArithmeticException("Length is zero, can't divide by zero");
+		} else {
+			this.x /= vecLength;
+			this.y /= vecLength;
+			this.z /= vecLength;
+		}
+	}
+
+	/**
+	 * Compares to a given vector
+	 * 
+	 * @param a
+	 *            Vector
+	 * @return True if have same values, false otherwise
+	 */
+	public boolean equals(Vec a) {
+		return ((a.x == x) && (a.y == y) && (a.z == z));
+	}
+
+	/**
+	 * Returns the angle in radians between this vector and the vector
+	 * parameter; the return value is constrained to the range [0,PI].
+	 * 
+	 * @param v1
+	 *            the other vector
+	 * @return the angle in radians in the range [0,PI]
+	 */
+	public final double angle(Vec v1) {
+		return Math.acos(dotProd(this, v1) / (this.length() * v1.length()));
+	}
+
+	/**
+	 * Computes the Euclidean distance between two points
+	 * 
+	 * @param a
+	 *            Point1
+	 * @param b
+	 *            Point2
+	 * @return Scalar
+	 */
+	static public double distance(Vec a, Vec b) {
+		return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2) + Math.pow(a.z - b.z, 2));
+	}
+
+	/**
+	 * Computes the cross product between two vectors using the right hand rule
+	 * 
+	 * @param a
+	 *            Vector1
+	 * @param b
+	 *            Vector2
+	 * @return Vector1 x Vector2
+	 */
+	public static Vec crossProd(Vec a, Vec b) {
+		return new Vec(b.y * a.z - b.z * a.y, b.z * a.x - b.x * a.z, b.x * a.y - b.y * a.x);
+	}
+
+	/**
+	 * Adds vectors a and b
+	 * 
+	 * @param a
+	 *            Vector
+	 * @param b
+	 *            Vector
+	 * @return a+b
+	 */
+	public static Vec add(Vec a, Vec b) {
+		b.x += a.x;
+		b.y += a.y;
+		b.z += a.z;
+		return b;
+	}
+
+	/**
+	 * Subtracts vector b from a
+	 * 
+	 * @param a
+	 *            Vector
+	 * @param b
+	 *            Vector
+	 * @return a-b
+	 */
+	public static Vec sub(Vec a, Vec b) {
+		a.x -= b.x;
+		a.y -= b.y;
+		a.z -= b.z;
+		return a;
+	}
+
+	/**
+	 * Inverses vector's direction
+	 * 
+	 * @param a
+	 *            Vector
+	 * @return -1*a
+	 */
+	public static Vec negate(Vec a) {
+		a.x *= -1;
+		a.y *= -1;
+		a.z *= -1;
+		return a;
+	}
+
+	/**
+	 * Scales vector a by scalar s
+	 * 
+	 * @param s
+	 *            Scalar
+	 * @param a
+	 *            Vector
+	 * @return s*a
+	 */
+	public static Vec scale(double s, Vec a) {
+		a.x *= s;
+		a.y *= s;
+		a.z *= s;
+		return a;
+	}
+
+	/**
+	 * Pair-wise scales vector a by vector b
+	 * 
+	 * @param a
+	 *            Vector
+	 * @param b
+	 *            Vector
+	 * @return a.*b
+	 */
+	public static Vec scale(Vec a, Vec b) {
+		return new Vec(a.x * b.x, a.y * b.y, a.z * b.z);
+	}
+
+	/**
+	 * Compares vector a to vector b
+	 * 
+	 * @param a
+	 *            Vector
+	 * @param b
+	 *            Vector
+	 * @return a==b
+	 */
+	public static boolean equals(Vec a, Vec b) {
+		return (a.x == b.x && a.y == b.y && a.z == b.z);
+	}
+
+	/**
+	 * Dot product of a and b
+	 * 
+	 * @param a
+	 *            Vector
+	 * @param b
+	 *            Vector
+	 * @return a.b
+	 */
+	public static double dotProd(Vec a, Vec b) {
+		return (a.x * b.x + a.y * b.y + a.z * b.z);
+	}
+
+	/**
+	 * Returns a string that contains the values of this vector. The form is
+	 * (x,y,z).
+	 * 
+	 * @return the String representation
+	 */
+	public String toString() {
+		return "(" + this.x + ", " + this.y + ", " + this.z + ")";
+	}
+
+	@Override
+	public Vec clone() {
+		return new Vec(this);
+	}
+}
