b09d03bd7dc71e1cc765c19f92e3e3a44f169057
Aviad Hahami <aviadhahami@gmail.com>
2015-03-27 17:27:59 +0300

    Heap implementation going up + started Ex #2 in graphics

--- DS/Ex03 - Heap/src/Heap.java
+++ DS/Ex03 - Heap/src/Heap.java
@@ -13,15 +13,12 @@ public class Heap {
 		this.heapSize = 1;
 	}
 
-	public void insert(Node node){
-		this.heapSize++;
-		int size = this.heapSize -1 ;
-		while (i > 0) {
-			type type = (type) en.nextElement();
-			
-		}
+	public void insert(Node node) {
+
 	}
-	
+
+
+
 	/* GETTERS AND SETTERS BELOW ! */
 
 	public Node getRoot() {
