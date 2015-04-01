31ed16dad4ecd040c08b11c177a24bebe05e8c41
Aviad Hahami <aviadhahami@gmail.com>
2015-04-01 22:49:13 +0300

    Updated the Ex

---
+++ DS/Ex03 - Heap/Homework3/ExtendedMaxHeap.java
@@ -0,0 +1,30 @@
+public class ExtendedMaxHeap {
+
+	public ExtendedMaxHeap(int capacity) {
+		
+	}
+
+	public ExtendedMaxHeap(HeapElement[] elementsArray, int capacity) 
+	
+	
+	public void insert(HeapElement e)
+	{
+		
+	}
+
+	public HeapElement deleteMax() throws HeapException
+	{
+		
+	}
+
+	public long getKeysAverage();
+	{
+		
+	}
+
+	public HeapElement getElementWithMinKey()
+	{
+		
+	}
+
+}
