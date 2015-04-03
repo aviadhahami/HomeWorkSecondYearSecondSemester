2481c9fa90f0bcd555bd7c0818f9ba57a034b4af
Aviad Hahami <aviadhahami@gmail.com>
2015-04-03 11:55:39 +0300

    Moving with stuff around the constructors

--- DS/Ex03 - Heap/Homework3/ExtendedMaxHeap.java
+++ DS/Ex03 - Heap/Homework3/ExtendedMaxHeap.java
@@ -1,30 +1,38 @@
 public class ExtendedMaxHeap {
 
+	private long keysAvg;
+	private HeapElement minKeyElement;
+	private HeapElement[] heap;
+
 	public ExtendedMaxHeap(int capacity) {
-		
+
 	}
 
-	public ExtendedMaxHeap(HeapElement[] elementsArray, int capacity) 
-	
-	
-	public void insert(HeapElement e)
-	{
-		
+	public ExtendedMaxHeap(HeapElement[] elementsArray, int capacity) {
+
 	}
 
-	public HeapElement deleteMax() throws HeapException
-	{
-		
+	public void insert(HeapElement e) {
+
 	}
 
-	public long getKeysAverage();
-	{
-		
+	public HeapElement deleteMax() throws HeapException {
+		if (this.heap[0] == null)
+			throw new HeapException("Empty queue, can't delete nothing");
+		HeapElement maxElement = this.heap[0];
+		// deletion logic comes here
+
+		return maxElement;
 	}
 
-	public HeapElement getElementWithMinKey()
-	{
-		
+	public long getKeysAverage() {
+		return this.keysAvg;
+	}
+
+	public HeapElement getElementWithMinKey() {
+		if (this.minKeyElement == null)
+			throw new HeapException("Queue is empty");
+		return this.minKeyElement;
 	}
 
 }
