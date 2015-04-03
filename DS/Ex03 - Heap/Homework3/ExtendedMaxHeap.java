3fa2c1bf0fd7538d0a887558f6380b18e6a3c325
Aviad Hahami <aviadhahami@gmail.com>
2015-04-03 11:44:14 +0300

    Revert "fuck this"
    This reverts commit 95f5ae2d6cc9b90afb5fb75aa857b42ee6afad5e.

--- DS/Ex03 - Heap/Homework3/ExtendedMaxHeap.java
+++ DS/Ex03 - Heap/Homework3/ExtendedMaxHeap.java
@@ -2,53 +2,22 @@ public class ExtendedMaxHeap {
 
 	private long keysAvg;
 	private HeapElement[] heap;
-	private HeapElement minKey;
 
 	public ExtendedMaxHeap(int capacity) {
 		// empty array with no data
 		this.heap = new HeapElement[capacity];
-		this.keysAvg = 0;
-		this.minKey = null;
+		maxHeapify();
+	}
+
+	private void maxHeapify() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	public ExtendedMaxHeap(HeapElement[] elementsArray, int capacity) {
 		this.heap = elementsArray;
-		/* forcing a minimal element --> will get sorted on the go */
-		this.minKey = this.heap[0]; 
-		buildMaxHeap();
-		this.keysAvg = this.keysAvg / this.heap.length;
-	}
+		maxHeapify();
 
-	private void buildMaxHeap() {
-		int heapSize = this.heap.length;
-		for (int i = heapSize / 2; i > 1; i--) {
-			MaxHeapify(i);
-		}
-	}
-
-	private void MaxHeapify(int i) {
-		int left = 2 * i;
-		int right = 2 * i + 1;
-		int bigger = i;
-		if (left <= this.heap.length && this.heap[left].getKey() > this.heap[bigger].getKey()) {
-			bigger = left;
-			this.minKey = this.minKey.getKey() < this.heap[left].getKey() ? this.minKey : this.heap[left];
-		}
-		if (right <= this.heap.length && this.heap[right].getKey() > this.heap[bigger].getKey()) {
-			bigger = right;
-			this.minKey = this.minKey.getKey() < this.heap[right].getKey() ? this.minKey : this.heap[right];
-		}
-		if (bigger != i) {
-			swap(i, bigger);
-			MaxHeapify(bigger);
-		}
-
-	}
-
-	private void swap(int i, int bigger) {
-		HeapElement temp = this.heap[i];
-		this.heap[i] = this.heap[bigger];
-		this.heap[bigger] = temp;
 	}
 
 	public void insert(HeapElement e) {
@@ -56,7 +25,6 @@ public class ExtendedMaxHeap {
 	}
 
 	public HeapElement deleteMax() throws HeapException {
-		return null;
 
 	}
 
@@ -65,7 +33,6 @@ public class ExtendedMaxHeap {
 	}
 
 	public HeapElement getElementWithMinKey() {
-		return null;
 
 	}
 
