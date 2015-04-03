91a13ee80f23560f11316180cee9e563aa3e5355
Aviad Hahami <aviadhahami@gmail.com>
2015-04-03 11:48:50 +0300

    Revert "started with constructors"
    This reverts commit 5788d65f8d0ff53a8af433c1c5a998afb065e059.

--- DS/Ex03 - Heap/Homework3/ExtendedMaxHeap.java
+++ DS/Ex03 - Heap/Homework3/ExtendedMaxHeap.java
@@ -1,39 +1,30 @@
 public class ExtendedMaxHeap {
 
-	private long keysAvg;
-	private HeapElement[] heap;
-
 	public ExtendedMaxHeap(int capacity) {
-		// empty array with no data
-		this.heap = new HeapElement[capacity];
-		maxHeapify();
-	}
-
-	private void maxHeapify() {
-		// TODO Auto-generated method stub
 		
 	}
 
-	public ExtendedMaxHeap(HeapElement[] elementsArray, int capacity) {
-		this.heap = elementsArray;
-		maxHeapify();
-
+	public ExtendedMaxHeap(HeapElement[] elementsArray, int capacity) 
+	
+	
+	public void insert(HeapElement e)
+	{
+		
 	}
 
-	public void insert(HeapElement e) {
-
+	public HeapElement deleteMax() throws HeapException
+	{
+		
 	}
 
-	public HeapElement deleteMax() throws HeapException {
-
+	public long getKeysAverage();
+	{
+		
 	}
 
-	public long getKeysAverage() {
-		return this.keysAvg;
-	}
-
-	public HeapElement getElementWithMinKey() {
-
+	public HeapElement getElementWithMinKey()
+	{
+		
 	}
 
 }
