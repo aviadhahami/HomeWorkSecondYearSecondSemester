2438328dc4c4cca40e4799f6ee8270c259125279
Aviad Hahami <aviadhahami@gmail.com>
2015-04-05 17:55:04 +0300

    Dafuq was this shit? jesus :o: :m: :goat:

---
+++ DS/Ex03 - Heap/Homework3/src/BankQueue.java
@@ -0,0 +1,31 @@
+public class BankQueue {
+	
+	
+	public BankQueue(int maxQueueSize){
+	}
+	
+	public BankQueue(PersonWithMoney [] people, int maxQueueSize){
+		
+	}
+	
+	public void enqueue(PersonWithMoney p){
+		
+	}
+	
+	public PersonWithMoney dequeue(){
+		
+	}
+	
+	public void stealFromFirstInQueue(int ammount){
+		
+	}
+
+	public int getAverageMoneyInQueue(){
+		
+	}
+	
+	public PersonWithMoney getPoorestPerson(){
+		
+	}
+	
+}
