025744bc16fb7b32ea8c0e5f7e3366a58f1f5b96
Aviad Hahami <aviadhahami@gmail.com>
2015-04-11 23:18:37 +0300

    work work

--- DS/Ex03 - Heap/Homework3/src/ExtendedMaxHeap.java
+++ DS/Ex03 - Heap/Homework3/src/ExtendedMaxHeap.java
@@ -1,30 +1,36 @@
 public class ExtendedMaxHeap {
+	private HeapElement[] heapArray;
+	private int maxSize; // size of array
+	private int currentSize; // number of nodes in array
 
 	public ExtendedMaxHeap(int capacity) {
-		
+		this.maxSize = capacity;
+		this.currentSize = 0;
+		this.heapArray = new HeapElement[capacity]; // create array
 	}
 
-	public ExtendedMaxHeap(HeapElement[] elementsArray, int capacity) 
-	
-	
-	public void insert(HeapElement e)
-	{
-		
+	public ExtendedMaxHeap(HeapElement[] elementsArray, int capacity) {
+
 	}
 
-	public HeapElement deleteMax() throws HeapException
-	{
-		
+	public void insert(HeapElement e) {
+
 	}
 
-	public long getKeysAverage();
-	{
-		
+	public HeapElement deleteMax() throws HeapException {
+
 	}
 
-	public HeapElement getElementWithMinKey()
-	{
-		
+	public long getKeysAverage() {
+
 	}
 
+	public HeapElement getElementWithMinKey() {
+
+	}
+
+	// custom methods
+	public boolean isEmpty() {
+		return this.currentSize == 0;
+	}
 }
