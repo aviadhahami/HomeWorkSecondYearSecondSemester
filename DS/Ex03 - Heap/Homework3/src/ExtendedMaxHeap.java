4e331266fe8b5b43693ab1ae3509f337aa2a47bf
Aviad Hahami <aviadhahami@gmail.com>
2015-04-11 23:28:56 +0300

    Keeping it up

--- DS/Ex03 - Heap/Homework3/src/ExtendedMaxHeap.java
+++ DS/Ex03 - Heap/Homework3/src/ExtendedMaxHeap.java
@@ -2,6 +2,7 @@ public class ExtendedMaxHeap {
 	private HeapElement[] heapArray;
 	private int maxSize; // size of array
 	private int currentSize; // number of nodes in array
+	private HeapElement minElement;
 
 	public ExtendedMaxHeap(int capacity) {
 		this.maxSize = capacity;
@@ -14,12 +15,57 @@ public class ExtendedMaxHeap {
 	}
 
 	public void insert(HeapElement e) {
+		if (this.currentSize != maxSize) {
+			// minelement init
+			minElement = minElement == null ? e : (minElement.getKey() > e.getKey() ? e : minElement);
 
+			HeapElement newElement = new HeapElement(e.getKey(), e.getData());
+			this.heapArray[this.currentSize] = newElement;
+			trickleUp(this.currentSize++);
+		}
+	}
+
+	public void trickleUp(int index) {
+		int parent = (index - 1) / 2;
+		HeapElement bottom = heapArray[index];
+
+		while (index > 0 && heapArray[parent].getKey() < bottom.getKey()) {
+			heapArray[index] = heapArray[parent]; // move it down
+			index = parent;
+			parent = (parent - 1) / 2;
+		}
+		heapArray[index] = bottom;
 	}
 
 	public HeapElement deleteMax() throws HeapException {
-
+		HeapElement root = heapArray[0];
+		heapArray[0] = heapArray[--currentSize];
+		trickleDown(0);
+		return root;
 	}
+
+	public void trickleDown(int index) {
+		int largerChild;
+		HeapElement top = heapArray[index]; // save root
+		while (index < currentSize / 2) // while node has at
+		{ // least one child,
+			int leftChild = 2 * index + 1;
+			int rightChild = leftChild + 1;
+			// find larger child
+			if (rightChild < currentSize && // (rightChild exists?)
+					heapArray[leftChild].getKey() < heapArray[rightChild].getKey())
+				largerChild = rightChild;
+			else
+				largerChild = leftChild;
+			// top >= largerChild?
+			if (top.getKey() >= heapArray[largerChild].getKey())
+				break;
+			// shift child up
+			heapArray[index] = heapArray[largerChild];
+			index = largerChild; // go down
+		} // end while
+		heapArray[index] = top; // root to index
+	} // end trickleDown()
 
 	public long getKeysAverage() {
 
