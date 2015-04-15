0ddeb5c986dd3faaa083e12cb90eb2adfb9e95a5
Aviad Hahami <aviadhahami@gmail.com>
2015-04-15 23:55:21 +0300

    shit

--- OS/Ex02/Copier.java
+++ OS/Ex02/Copier.java
@@ -1,65 +1,83 @@
+/*
+ * Name: Aviya Sela
+ * ID No: 302221403
+ */
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 public class Copier implements Runnable {
-	private File destFile;
-	private String filePath;
-	private SynchronizedQueue<File> resultQue;
+
+	private SynchronizedQueue<File> resultsQue;
+	private File fileDest;
+
+	// Size of buffer used for a single file copy process
+	private static final int COPY_BUFFER_SIZE = 4096;
 
 	/**
-	 * 
-	 * @param resultQue
-	 *            - Queue of results
-	 * 
-	 * @param destFile
-	 *            - destination file
+	 * Constructor. 
+	 * Initializes the worker with a destination directory and a queue of files to copy. 
+	 * @param destination - Destination directory
+	 * @param results - Queue of files found, to be copied
 	 */
-	public Copier(File destFile, SynchronizedQueue<File> resultQue) {
-		this.filePath = destFile.getAbsolutePath();
-		this.destFile = destFile;
-		this.resultQue = resultQue;
+	public Copier(File destination, SynchronizedQueue<File> results) {
+		resultsQue = results;
+		fileDest = destination;
 	}
 
+	/**
+	 * Runs the copier thread.
+	 * 
+	 * Thread will fetch files from queue and copy them, one after each other, 
+	 * to the destination directory.
+	 *  
+	 *  When the queue has no more files, the thread finishes.
+	 */
 	@Override
 	public void run() {
-		File destinationFile;
-		File f;
-		while ((f = resultQue.dequeue()) != null) {
+
+		File currentFile;
+		File newFile;
+		int size;
+
+		while ((currentFile = resultsQue.dequeue()) != null) {
+
+			// Sets the ground for try phrase
+			FileInputStream input = null;
+			FileOutputStream output = null;
+			byte[] buffer = new byte[COPY_BUFFER_SIZE];
+
+			String path = fileDest.getAbsolutePath();
+			newFile = new File(path, currentFile.getName());
+
 			try {
-				destinationFile = new File(this.filePath, f.getName());
-				copyFileContent(f, destinationFile);
-			} catch (Exception e) {
-				// we go on...
+				input = new FileInputStream(currentFile);
+				output = new FileOutputStream(newFile);
+
+				// Copies file one buffer at a time
+				while ((size = input.read(buffer, 0, COPY_BUFFER_SIZE)) > -1) {
+					output.write(buffer, 0, size);
+				}
+			} catch (IOException e) {
+				// Skips problem as directed 
+				
+			} finally {
+
+				// Closes streams
+				try {
+					if (input != null) {
+						input.close();
+					}
+					if (output != null) {
+						output.close();
+					}
+				} catch (Exception e) {
+					System.err.println("I/O Streams didn't close properly: " + e.getMessage());
+				}
+
 			}
-
-		}
-
-	}
-
-	private void copyFileContent(File sourceFile, File destinationFile) throws IOException {
-		int byteBufferSize = 2048;
-		int length = 0;
-		// file I/O streams
-		InputStream fIS = null;
-		OutputStream fOS = null;
-		byte[] bufferSize = new byte[byteBufferSize];
-		try {
-			fIS = new FileInputStream(sourceFile);
-			fOS = new FileOutputStream(destinationFile);
-			while ((length = fIS.read(bufferSize)) > 0) {
-				fOS.write(bufferSize, 0, length);
-
-			}
-		} catch (Exception e) {
-			// TODO: handle exception
-		} finally {
-			// closing files
-			fIS.close();
-			fOS.close();
 		}
 	}
 }
