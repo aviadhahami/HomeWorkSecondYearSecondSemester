b058350ce6159d4cddb085baeabf9bc50cc2d5b2
Aviad Hahami <aviadhahami@gmail.com>
2015-04-16 00:35:56 +0300

    Revert "Aviya"
    This reverts commit d996546f4dab46b6f177887546c49e1dcb7d8ac6.

--- OS/Ex02/DiskSearcher.java
+++ OS/Ex02/DiskSearcher.java
@@ -1,132 +1,87 @@
-/*
- * Name: Aviya Sela
- * ID No: 302221403
- */
-
 import java.io.File;
 
+/**
+ * Main application class. This application searches for all files under some
+ * given path that contain a given textual pattern. All files found are copied
+ * to some specific directory.
+ */
 public class DiskSearcher {
 
-	// Capacity of the queue that holds the directories to be searched
-	public static final int DIRECTORY_QUEUE_CAPACITY = 1000;
+	private static final int DIRECTORY_QUEUE_CAPACITY = 1000;
+	private static final int RESULTS_QUEUE_CAPACITY = 1000;
 
-	// Capacity of the queue that holds the files found
-	public static final int RESULTS_QUEUE_CAPACITY = 1000;
-
-	private static final String USAGE = "Usage: java DiskSearcher " + "<extension> <root directory> <destination directory>" + "<# of searchers>"
-			+ "<# of copiers>";
-
-	/**
-	 * Main method. Reads arguments from command line and starts the search.
-	 * 
-	 * @param args
-	 *            - Command line arguments
-	 */
 	public static void main(String[] args) {
+		int o_numberOfSearchThreads = 0;
+		int o_numberOfCopyThreads = 0;
 
-		// Validates number of arguments
 		if (args.length != 5) {
-			System.out.println(USAGE);
+			System.err.println("Wrong number of args! expected " + 5 + " but recieved " + args.length);
 			System.exit(1);
 		}
 
-		// Gets all of the Arguments
-		String extension = args[0];
-		File root = new File(args[1]);
-		File destination = new File(args[2]);
-		int numOfSearcher = 0;
-		int numOfCopier = 0;
+		// catching incoming arguments
+		String o_fileExtention = args[0];
+		File o_rootFolder = new File(args[1]);
+		File o_destFolder = new File(args[2]);
 
-		// Validates digits input
 		try {
-			numOfSearcher = Integer.parseInt(args[3]);
-			numOfCopier = Integer.parseInt(args[4]);
-		} catch (Exception e) {
-			System.out.println(USAGE);
-			System.exit(2); // Does not consist of digits
+			o_numberOfSearchThreads = Integer.parseInt(args[3]);
+			o_numberOfCopyThreads = Integer.parseInt(args[4]);
+		} catch (NumberFormatException e) {
+			System.err.println("Expected two numbers, got sh*t instead");
+			System.exit(1);
+		}
+		if (o_numberOfCopyThreads < 1 || o_numberOfSearchThreads < 1) {
+			System.err.println("Negative number, negative attitude -> you're out ! ");
+			System.exit(1);
 		}
 
-		// Validates amount input
-		if (numOfSearcher < 1 || numOfCopier < 1) {
-			System.out.println(USAGE);
-			System.exit(3); // Too few number of threads
+		// if no destination folder we generate it
+		if (!o_destFolder.isDirectory()) {
+			o_destFolder.mkdir();
 		}
 
-		// Validates directory input
-		if (!destination.isDirectory()) {
-			destination.mkdir();
+		// initializing queues
+		SynchronizedQueue<File> o_directoryQueue = new SynchronizedQueue<File>(DIRECTORY_QUEUE_CAPACITY);
+		SynchronizedQueue<File> o_resultsQueue = new SynchronizedQueue<File>(RESULTS_QUEUE_CAPACITY);
+
+		// gogo gadget - > SCOUT
+		Scouter o_scouter = new Scouter(o_directoryQueue, o_rootFolder);
+		Thread o_scoutingThread = new Thread(o_scouter);
+		o_scoutingThread.start();
+
+		// put water in the pools
+		Thread[] io_searchersPool = new Thread[o_numberOfSearchThreads];
+		Thread[] io_copiersPool = new Thread[o_numberOfCopyThreads];
+
+		// initialize search threads sequence
+		for (int i = 0; i < o_numberOfSearchThreads; i++) {
+			io_searchersPool[i] = new Thread(new Searcher(o_fileExtention, o_resultsQueue, o_directoryQueue));
+			// DEOPLOY THE SENTINELS!
+			io_searchersPool[i].start();
 		}
 
-		// Starts the search
-		search(extension, root, destination, numOfSearcher, numOfCopier);
-
-	}
-
-	/**
-	 * Performs the search with Scouter, Searcher and Copier, according with the
-	 * given command lines:
-	 * 
-	 * @param extension
-	 *            - The file extension to be searched.
-	 * @param root
-	 *            - The directory to start searching from.
-	 * @param destination
-	 *            - The directory to copy all files to.
-	 * @param searchThreads
-	 *            - Number of Search threads to use.
-	 * @param copierThreads
-	 *            - Number of Copier threads to use.
-	 */
-	private static void search(String extension, File root, File destination, int numOfSearcher, int numOfCopier) {
-
-		SynchronizedQueue<File> directories = new SynchronizedQueue<File>(DIRECTORY_QUEUE_CAPACITY);
-		SynchronizedQueue<File> results = new SynchronizedQueue<File>(RESULTS_QUEUE_CAPACITY);
-
-		Searcher[] searchers = new Searcher[numOfSearcher];
-		Thread[] searcherThreads = new Thread[numOfSearcher];
-
-		Copier[] copiers = new Copier[numOfCopier];
-		Thread[] copierThreads = new Thread[numOfCopier];
-
-		Scouter scouter = new Scouter(directories, root);
-		Thread scouterThread = new Thread(scouter);
-
-		scouterThread.start();
-
-		// Starts all Searcher Threads
-		for (int i = 0; i < numOfSearcher; i++) {
-			searchers[i] = new Searcher(extension, directories, results);
-			searcherThreads[i] = new Thread(searchers[i]);
-			searcherThreads[i].start();
-			System.out.println("search thread started, id " + searcherThreads[i].getName());
+		// initialize copy threads sequence
+		for (int i = 0; i < o_numberOfCopyThreads; i++) {
+			io_copiersPool[i] = new Thread(new Copier(o_destFolder, o_resultsQueue));
+			// DEOPLOY THE SENTINELS!
+			io_copiersPool[i].start();
 		}
 
-		// Starts all Copier Threads
-		for (int i = 0; i < numOfCopier; i++) {
-			copiers[i] = new Copier(destination, results);
-			copierThreads[i] = new Thread(copiers[i]);
-			copierThreads[i].start();
-			System.out.println("copy thread started, id " + copierThreads[i].getName());
-		}
-
-		// Waits for all threads to finish
 		try {
-
-			scouterThread.join();
-
-			for (int i = 0; i < numOfSearcher; i++) {
-				searcherThreads[i].join();
+			// prepare to die threads !
+			o_scoutingThread.join();
+			for (int i = 0; i < io_searchersPool.length; i++) {
+				io_searchersPool[i].join();
 			}
-
-			for (int i = 0; i < numOfCopier; i++) {
-				copierThreads[i].join();
+			// are you dead?
+			for (int i = 0; i < io_copiersPool.length; i++) {
+				io_copiersPool[i].join();
 			}
 		} catch (InterruptedException e) {
-			System.err.println(e);
+			e.printStackTrace();
 		}
-
-		System.out.println("Good God!");
-
+		System.out.println("VICTORY! ALL YOUR BASE ARE BELONG TO US!");
 	}
 
 }
--- OS/Ex02/SynchronizedQueue.java
+++ OS/Ex02/SynchronizedQueue.java
@@ -1,111 +1,113 @@
-/*
- * Name: Aviya Sela
- * ID No: 302221403
- */
-
 /**
- * A synchronized bounded-size queue for multithreaded producer-consumer applications.
+ * A synchronized bounded-size queue for multithreaded producer-consumer
+ * applications.
  * 
- * @param <T> Type of data items
+ * @param <T>
+ *            Type of data items
  */
 public class SynchronizedQueue<T> {
 
 	private T[] buffer;
 	private int producers;
-	private int capacity;
-	private int start;
-	private int size; 
+	// custom private members
+	private int i_size;
+	private int i_startingPoint;
+	private int i_producers;
+	private int i_capacity;
+
+	// locking synchronizer
+	private Object queLocker;
 
 	/**
 	 * Constructor. Allocates a buffer (an array) with the given capacity and
 	 * resets pointers and counters.
-	 * @param capacity Buffer capacity
+	 * 
+	 * @param capacity
+	 *            Buffer capacity
 	 */
 	@SuppressWarnings("unchecked")
 	public SynchronizedQueue(int capacity) {
-		this.buffer = (T[])(new Object[capacity]);
-		this.producers = 0;
-		this.capacity = 0;
-		this.start = 0;
-		this.size = 0;
+		// Standard constructor
+		this.buffer = (T[]) (new Object[capacity]);
+		i_startingPoint = 0;
+		producers = 0;
+		i_size = 0;
+		// local object
+		this.i_capacity = capacity;
+		queLocker = new Object();
 	}
 
 	/**
-	 * Dequeues the first item from the queue and returns it.
-	 * If the queue is empty but producers are still registered to this queue, 
-	 * this method blocks until some item is available.
-	 * If the queue is empty and no more items are planned to be added to this 
-	 * queue (because no producers are registered), this method returns null.
+	 * Dequeues the first item from the queue and returns it. If the queue is
+	 * empty but producers are still registered to this queue, this method
+	 * blocks until some item is available. If the queue is empty and no more
+	 * items are planned to be added to this queue (because no producers are
+	 * registered), this method returns null.
 	 * 
 	 * @return The first item, or null if there are no more items
 	 * @see #registerProducer()
 	 * @see #unregisterProducer()
 	 */
-	public synchronized T dequeue() {
-
-		while (size == 0) {
-
-			if (producers == 0) {
-				return null;
+	public T dequeue() {
+		synchronized (queLocker) {
+			while (i_size == 0) {
+				if (producers == 0) {
+					return null;
+				}
+				try {
+					queLocker.wait();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
-
-			// Waits for registered producers
-			try {
-				this.wait();
-			} catch (InterruptedException e) {
-				System.err.println(e);
-			}
-
+			T i_item = buffer[i_startingPoint];
+			i_size--;
+			i_startingPoint = (++i_startingPoint) % i_capacity;
+			queLocker.notifyAll();
+			return i_item;
 		}
 
-		T item = buffer[start];
-
-		// Maintains size and start 
-		start = (start + 1) % capacity; 
-		size--;
-
-		this.notifyAll();
-
-		return item;
 	}
 
 	/**
-	 * Enqueues an item to the end of this queue. If the queue is full, this 
+	 * Enqueues an item to the end of this queue. If the queue is full, this
 	 * method blocks until some space becomes available.
 	 * 
-	 * @param item Item to enqueue
+	 * @param item
+	 *            Item to enqueue
 	 */
-	public synchronized void enqueue(T item) {
+	public void enqueue(T item) {
+		synchronized (queLocker) {
+			while (i_size == i_capacity) {
+				try {
+					queLocker.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 
-		while (size == capacity) {
-			try {
-				this.wait();
-			} catch (InterruptedException e) {
-				System.err.println(e);
 			}
+			buffer[(i_startingPoint + i_size) % i_capacity] = item;
+			i_size++;
+			queLocker.notifyAll();
 		}
-		
-		buffer[(start + size) % capacity] = item;
-		
-		size++;
-		
-		this.notifyAll();
 	}
 
 	/**
 	 * Returns the capacity of this queue
+	 * 
 	 * @return queue capacity
 	 */
 	public int getCapacity() {
-		return capacity;
+		return i_capacity;
 	}
 
 	/**
 	 * Returns the current size of the queue (number of elements in it)
+	 * 
 	 * @return queue size
 	 */
 	public int getSize() {
-		return size;
+		return i_size;
 	}
 
 	/**
@@ -113,10 +115,10 @@ public class SynchronizedQueue<T> {
 	 * internal producers counter of this queue by 1. This counter is used to
 	 * determine whether the queue is still active and to avoid blocking of
 	 * consumer threads that try to dequeue elements from an empty queue, when
-	 * no producer is expected to add any more items.
-	 * Every producer of this queue must call this method before starting to 
-	 * enqueue items, and must also call <see>{@link #unregisterProducer()}</see> when
-	 * finishes to enqueue all items.
+	 * no producer is expected to add any more items. Every producer of this
+	 * queue must call this method before starting to enqueue items, and must
+	 * also call <see>{@link #unregisterProducer()}</see> when finishes to
+	 * enqueue all items.
 	 * 
 	 * @see #dequeue()
 	 * @see #unregisterProducer()
@@ -126,12 +128,19 @@ public class SynchronizedQueue<T> {
 	}
 
 	/**
-	 * Unregisters a producer from this queue. See <see>{@link #registerProducer()}</see>.
+	 * Unregisters a producer from this queue. See <see>
+	 * {@link #registerProducer()}</see>.
 	 * 
 	 * @see #dequeue()
 	 * @see #registerProducer()
 	 */
 	public synchronized void unregisterProducer() {
-		this.producers--;
+		if (producers > 0) {
+			this.producers--;
+			synchronized (queLocker) {
+				queLocker.notifyAll();
+			}
+		}
+
 	}
 }
