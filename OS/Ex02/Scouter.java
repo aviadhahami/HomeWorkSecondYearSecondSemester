fab8cb97c37720dfcad46b50da2599d15cd4c0c8
Aviad Hahami <aviadhahami@gmail.com>
2015-04-15 19:46:32 +0300

    supposely done with scouter

--- OS/Ex02/Scouter.java
+++ OS/Ex02/Scouter.java
@@ -1,10 +1,40 @@
+import java.io.File;
+import java.util.LinkedList;
 
-public class Scouter implements Runnable{
+public class Scouter implements Runnable {
+
+	private File i_headDir;
+	private LinkedList<File> i_scoutedItemsList;
+	private SynchronizedQueue<File> o_directoryQue;
+
+	public Scouter(SynchronizedQueue<File> dirQue, File startingPoint) {
+		i_headDir = startingPoint;
+		o_directoryQue = dirQue;
+
+		i_scoutedItemsList = new LinkedList<File>();
+		o_directoryQue.registerProducer();
+	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		File[] i_filesArray;
+		File i_currentDirectory;
+
+		o_directoryQue.enqueue(i_headDir);
+		i_scoutedItemsList.addLast(i_headDir);
+
+		while ((i_currentDirectory = i_scoutedItemsList.poll()) != null) {
+			i_filesArray = i_currentDirectory.listFiles();
+			if (i_filesArray != null) {
+				for (File i_currFileInArr : i_filesArray) {
+					if (i_currFileInArr.isDirectory()) {
+						o_directoryQue.enqueue(i_currFileInArr);
+						i_scoutedItemsList.addLast(i_currFileInArr);
+					}
+				}
+			}
+		}
+		o_directoryQue.unregisterProducer();
 	}
 
 }
--- OS/Ex02/Searcher.java
+++ OS/Ex02/Searcher.java
@@ -2,14 +2,14 @@ import java.io.File;
 
 public class Searcher implements Runnable {
 	private String i_fileExtension;
-	private SynchronizedQueue<File> i_directoryQue;
+	private SynchronizedQueue<File> o_directoryQue;
 	private SynchronizedQueue<File> o_resultsQue;
 
 	public Searcher(SynchronizedQueue<File> dirQ, SynchronizedQueue<File> resQ, String fEx) {
 		// initialize private variables
 		i_fileExtension = fEx;
 		o_resultsQue = resQ;
-		i_directoryQue = dirQ;
+		o_directoryQue = dirQ;
 
 		o_resultsQue.registerProducer();
 	}
@@ -19,7 +19,7 @@ public class Searcher implements Runnable {
 		File i_currentDir;
 		File[] i_directoryFilesArray;
 		int fileEndingPos;
-		while ((i_currentDir = i_directoryQue.dequeue()) != null) {
+		while ((i_currentDir = o_directoryQue.dequeue()) != null) {
 			i_directoryFilesArray = i_currentDir.listFiles();
 			if (i_directoryFilesArray != null) {
 				for (int i = 0; i < i_directoryFilesArray.length; i++) {
