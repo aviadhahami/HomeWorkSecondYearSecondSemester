3ba2a406f5c9b73ae416bb895bc460f403af4bc9
Aviad Hahami <aviadhahami@gmail.com>
2015-05-17 02:08:16 +0300

    Working on the bucket

--- DS/Ex-05/BucketSort.java
+++ DS/Ex-05/BucketSort.java
@@ -8,18 +8,30 @@ import java.util.LinkedList;
 
 public class BucketSort implements Sort {
 
-	public int[] sort(int[] ar) {
+	public int[] sort(int[] io_InputArray) {
+
+		int i_ArrayMinimalValue = io_InputArray[0];
+		int i_ArrayMaximalValue = io_InputArray[0];
+
+		for (int i_CurrentNumberFromInput : io_InputArray) {
+			i_ArrayMinimalValue = Math.min(i_ArrayMinimalValue, i_CurrentNumberFromInput);
+			i_ArrayMaximalValue = Math.max(i_ArrayMaximalValue, i_CurrentNumberFromInput);
+		}
 
 		ArrayList<LinkedList<Integer>> buckets = new ArrayList<LinkedList<Integer>>();
 
 		// Creates new ar.length buckets
-		for (int i = 0; i < ar.length; i++) {
+		for (int i = 0; i < io_InputArray.length; i++) {
 			buckets.add(new LinkedList<Integer>());
 		}
 
 		// Sorts each value to its destined bucket
-		for (int i = 0; i < ar.length; i++) {
-			buckets.get((int) (ar[i] * ar.length)).add(ar[i]);
+		int i_PositionFormula;
+		int i_ArraySize = io_InputArray.length;
+
+		for (int i = 0; i < io_InputArray.length; i++) {
+			i_PositionFormula = ((io_InputArray[i] - i_ArrayMinimalValue) / i_ArrayMaximalValue) * i_ArraySize;
+			buckets.get(i_PositionFormula).add(io_InputArray[i]);
 		}
 
 		// Sorts each bucket, using Insertion Sort
@@ -40,10 +52,9 @@ public class BucketSort implements Sort {
 			insertion.sort(tempBucket);
 
 			// Concatenates the buckets into one array
-			System.arraycopy(tempBucket, 0, ar, cur, buckets.get(i).size());
+			System.arraycopy(tempBucket, 0, io_InputArray, cur, buckets.get(i).size());
 			cur += buckets.get(i).size();
 		}
-		return ar;
+		return io_InputArray;
 	}
-
 }
