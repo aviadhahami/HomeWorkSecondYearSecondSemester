ecdf783bca3ec48185513928a2c746b7ae83c394
Aviad Hahami <aviadhahami@gmail.com>
2015-05-20 16:44:28 +0300

    implemented wheel

--- C#/Ex3/B15 EX3 AVIADHAHAMI ID OMERWINTER ID/Vehicle.cs
+++ C#/Ex3/B15 EX3 AVIADHAHAMI ID OMERWINTER ID/Vehicle.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Collections.Generic;
 
 namespace B15_EX3_AVIADHAHAMI_ID_OMERWINTER_ID
 {
     class Vehicle
     {
+        string m_Model;
+        string m_LicenseNumber;
+        float m_RemainingEnery;
+        List<Wheel> m_Wheels;
     }
 }
---
+++ C#/Ex3/B15 EX3 AVIADHAHAMI ID OMERWINTER ID/Wheel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace B15_EX3_AVIADHAHAMI_ID_OMERWINTER_ID
+{
+    class Wheel
+    {
+        string m_Manufacturer;
+        float m_CurrentAirPressure;
+        float m_MaximalAirPressure;
+        public void Wheel(string i_Manufacturer, float i_CurrentAirPressure, float i_MaximalAirPressure)
+        {
+            m_CurrentAirPressure = i_CurrentAirPressure;
+            m_Manufacturer = i_Manufacturer;
+            m_MaximalAirPressure = i_MaximalAirPressure;
+        }
+
+        public float PumpAir
+        {
+            set
+            {
+                if (value > 0 && value + m_CurrentAirPressure <= m_MaximalAirPressure)
+                {
+                    m_CurrentAirPressure += value;
+                }
+                else
+                {
+                    // Should delete this else when done
+                    Console.WriteLine("Didnt add air, exceeded max volume");
+                }
+            }
+        }
+    }
+}
