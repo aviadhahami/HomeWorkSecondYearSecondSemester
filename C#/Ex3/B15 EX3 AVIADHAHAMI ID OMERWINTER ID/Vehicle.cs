652b4c9b7c042f01b490d6f68f233480988a454a
Aviad Hahami <aviadhahami@gmail.com>
2015-05-20 16:50:00 +0300

    Generated members for Vehicle

--- C#/Ex3/B15 EX3 AVIADHAHAMI ID OMERWINTER ID/Vehicle.cs
+++ C#/Ex3/B15 EX3 AVIADHAHAMI ID OMERWINTER ID/Vehicle.cs
@@ -9,7 +9,17 @@ namespace B15_EX3_AVIADHAHAMI_ID_OMERWINTER_ID
     {
         string m_Model;
         string m_LicenseNumber;
-        float m_RemainingEnery;
+        float m_RemainingEnergy;
         List<Wheel> m_Wheels;
+
+        public void Vehicle(string i_Model, string i_LicenseNumber, float i_RemainingEnergy, List<Wheel> i_Wheels)
+        {
+            m_Model = i_Model;
+            m_LicenseNumber = i_LicenseNumber;
+            m_RemainingEnergy = i_RemainingEnergy;
+            m_Wheels = i_Wheels;
+        }
+
     }
+
 }
