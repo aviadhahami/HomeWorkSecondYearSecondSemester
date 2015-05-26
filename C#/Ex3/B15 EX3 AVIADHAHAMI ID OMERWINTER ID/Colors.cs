64197eed12796abe96f4d1db6a44382554d69798
shweps <omer.winter11@gmail.com>
2015-05-26 21:36:11 +0300

    add some details
    work on polimorphisem

--- C#/Ex3/B15 EX3 AVIADHAHAMI ID OMERWINTER ID/Colors.cs
+++ C#/Ex3/B15 EX3 AVIADHAHAMI ID OMERWINTER ID/Colors.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace B15_EX3_AVIADHAHAMI_ID_OMERWINTER_ID
 {
-    class Truck : Vehicle
+    public enum Colors
     {
-
-    }
+        GREEN,
+        RED,
+        BLACK,
+        WIGHT
+    };
 }
