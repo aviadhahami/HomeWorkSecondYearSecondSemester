30080457ec2a7dddcbc973d784927fb369e22c44
Aviad Hahami <aviadhahami@gmail.com>
2015-05-26 20:52:21 +0300

    IGNORE THIS COMMIT !

---
+++ C#/Ex3/Ex03.GarageManagmentSystem.ConsoleUI/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageManagmentSystem.ConsoleUI
+{
+    class Main
+    {
+        Console console;
+        public void main()
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
