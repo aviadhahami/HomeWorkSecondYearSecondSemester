2738e6b57d36c8eb32fcef2eea2d79fd7441844c
Aviad Hahami <aviadhahami@gmail.com>
2015-05-26 21:32:15 +0300

    Working on UI

--- C#/Ex3/Ex03.GarageManagmentSystem.ConsoleUI/Program.cs
+++ C#/Ex3/Ex03.GarageManagmentSystem.ConsoleUI/Program.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using B15_EX3_AVIADHAHAMI_ID_OMERWINTER_ID;
 
 namespace Ex03.GarageManagmentSystem.ConsoleUI
 {
     class ConsoleUI
     {
-        static void UIGenerator(string[] args)
+        UITexts m_UIStrings;
+        static void Main()
         {
-          
+            // Instansiate the UI
+            UITexts m_UIStrings = new UITexts();
+            
+            // Greet the mofos
+            m_UIStrings.sayHi();
+            m_UIStrings.HoldScreen();
         }
     }
 }
---
+++ C#/Ex3/Ex03.GarageManagmentSystem.ConsoleUI/UITexts.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex03.GarageManagmentSystem.ConsoleUI
+{
+    class UITexts
+    {
+        private const string k_GREETINGS = "Hi, Welcome to our garage!";
+        private const string k_DECORATED_LINE_SEPARATOR = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+
+        internal void sayHi()
+        {
+            Console.WriteLine(k_GREETINGS);
+            Console.WriteLine(k_DECORATED_LINE_SEPARATOR);
+        }
+
+        internal void HoldScreen()
+        {
+            Console.WriteLine("Press any key to continue");
+            Console.Read();
+        }
+    }
+}
