8ae68dd570728331a43c0c58e9b89f2cee48b06e
Aviad Hahami <aviadhahami@gmail.com>
2015-06-01 14:31:01 +0300

    Working on implementation... Jesus im falling asleep

--- C#/Ex3/Ex03.GarageManagmentSystem.ConsoleUI/ConsoleUi.cs
+++ C#/Ex3/Ex03.GarageManagmentSystem.ConsoleUI/ConsoleUi.cs
@@ -25,9 +25,14 @@ namespace Ex03.GarageManagmentSystem.ConsoleUI
         // DisplaySingleVehicle
 
         //Logout
+
+        private const string k_EXIT_TOKEN = "EXIT";
+
+
         UITexts m_UITexts;
         string m_CurrentUserName;
         string m_CurrentUserPhone;
+
 
         public ConsoleUI()
         {
@@ -42,7 +47,80 @@ namespace Ex03.GarageManagmentSystem.ConsoleUI
 
             m_CurrentUserPhone = requireUserPhone();
 
-            showMainMenu();
+            invokeMainMenuSequence();
+        }
+
+        private string requireUserPhone()
+        {
+            string o_UserInput;
+            while (true)
+            {
+                o_UserInput = m_UITexts.AskUserForPhone();
+                if (o_UserInput.Length > 0)
+                {
+                    if (CheckExitToken(o_UserInput))
+                    {
+                        leaveApplication();
+                    }
+                    else if (containsDigitsOnly(o_UserInput))
+                    {
+                        return o_UserInput;
+                    }
+                    else
+                    {
+                        m_UITexts.BadInput();
+                    }
+                }
+            }
+        }
+
+        private string requireUserName()
+        {
+            string o_UserInput;
+            while (true)
+            {
+                o_UserInput = m_UITexts.AskUserForName();
+                if (o_UserInput.Length > 0)
+                {
+                    if (CheckExitToken(o_UserInput))
+                    {
+                        leaveApplication();
+                    }
+                    else if (containsLettersAndSpacesOnly(o_UserInput))
+                    {
+                        return o_UserInput;
+                    }
+                    else
+                    {
+                        m_UITexts.BadInput();
+                    }
+                }
+            }
+        }
+
+        private bool containsLettersAndSpacesOnly(string o_UserInput)
+        {
+            bool o_testResult = true;
+            foreach (char currentChar in o_UserInput)
+            {
+                // If the current char is not a letter neither a whitespace
+                if (!char.IsLetter(currentChar) && !char.IsWhiteSpace(currentChar))
+                {
+                    o_testResult = false;
+                }
+            }
+            return o_testResult;
+        }
+
+        private void leaveApplication()
+        {
+            m_UITexts.SayGoodbye();
+            Environment.Exit(0);
+        }
+
+        private bool CheckExitToken(string o_UserInput)
+        {
+            return o_UserInput.ToUpper() == k_EXIT_TOKEN;
         }
 
     }
