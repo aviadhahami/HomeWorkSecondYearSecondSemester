132879e9660858a9c7228d8df6e613404bd598bc
Aviad Hahami <aviadhahami@gmail.com>
2015-06-03 10:42:34 +0300

    Generated new project for EX4

---
+++ C#/B15 Ex04 OmerWinter 305526907 AviadHahami 302188347/B15 Ex04 OmerWinter 305526907 AviadHahami 302188347/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B15_Ex04_OmerWinter_305526907_AviadHahami_302188347
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
