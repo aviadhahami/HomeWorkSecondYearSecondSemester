6344dfd6ccea197467079a498bc709485997180d
Aviad Hahami <aviadhahami@gmail.com>
2015-06-03 14:25:00 +0300

    Implemented classes

---
+++ C#/B15 Ex04 OmerWinter 305526907 AviadHahami 302188347/Ex04.Menus. Interfaces/InfoMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Ex04.Menus.Interfaces
+{
+    class InfoMenu : IInfo
+    {
+        private const string k_VERSION_STRING = "Version: 15.2.4.0";
+
+        public void ShowVersion()
+        {
+            Console.WriteLine(k_VERSION_STRING);
+        }
+
+        public void CountWords()
+        {
+            Console.WriteLine("Please enter a string");
+            string userInput = Console.ReadLine();
+            MatchCollection collection = Regex.Matches(userInput, @"[\S]+");
+            Console.WriteLine("Amount of words: " + collection.Count);
+        }
+    }
+}
---
+++ C#/B15 Ex04 OmerWinter 305526907 AviadHahami 302188347/Ex04.Menus. Interfaces/ShowDateOrTimeMenue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex04.Menus.Interfaces
+{
+    class ShowDateOrTimeMenue : IShowDateOrTime
+    {
+        public void ShowTime()
+        {
+            Console.WriteLine(DateTime.Now.ToString("h:mm:ss tt"));
+        }
+
+        public void ShowDate()
+        {
+            Console.WriteLine(DateTime.Now.ToString("d"));
+        }
+    }
+}
