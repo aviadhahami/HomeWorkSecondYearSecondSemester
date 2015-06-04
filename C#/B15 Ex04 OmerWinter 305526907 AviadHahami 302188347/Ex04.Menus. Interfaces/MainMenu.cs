881fbd0470a1fbaa888ef283dc710462b85b2877
Aviad Hahami <aviadhahami@gmail.com>
2015-06-04 21:53:32 +0300

    done with first half (Interfaces)

--- C#/B15 Ex04 OmerWinter 305526907 AviadHahami 302188347/Ex04.Menus. Interfaces/MainMenu.cs
+++ C#/B15 Ex04 OmerWinter 305526907 AviadHahami 302188347/Ex04.Menus. Interfaces/MainMenu.cs
@@ -7,99 +7,131 @@ namespace Ex04.Menus.Interfaces
 {
     public class MainMenu
     {
+        private const string k_WRONG_INPUT_TYPE = "Please type a proper choice index or 'exit' to leave";
+        private const string k_HEADER_SEPARATOR = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+        private const string k_PLEASE_PICK_AN_INDEX = "Please pick a choice's index or 'exit' to leave";
+        private const string k_SAY_BYE = "Thank you and goodbye!";
+        private const string k_NO_SUCH_OPTION = "There isn't such an option, retry";
+        private const string k_CONTACT_ADMIN = "Something happened, please contant support";
+        private const string k_EXIT_TOKEN = "EXIT";
+
+        // Classes members
         private InfoMenu m_InfoMenu;
         private ShowDateOrTimeMenu m_DateOrTimeMenu;
 
         public MainMenu()
         {
+            // Call relevant methods
             m_InfoMenu = new InfoMenu();
             m_DateOrTimeMenu = new ShowDateOrTimeMenu();
         }
+
+        // Main show method
         public void Show()
         {
-            object[] test = { m_DateOrTimeMenu, m_InfoMenu };
-            string userInput;
-            int parsedVal = 0;
+            object[] classesArray = { m_DateOrTimeMenu, m_InfoMenu };
+            string userInput = "";
+            int parsedVal = -1;
             bool parseFlag = false;
+            // use 'this' for first iteration
             object currentMethod = this;
             while (true)
             {
                 Console.WriteLine(currentMethod.GetType().Name);
-                Console.WriteLine("~~~~~~~~~~~~~~~~~~");
-                for (int i = 0; i < test.Length; i++)
+                Console.WriteLine(k_HEADER_SEPARATOR);
+                for (int i = 0; i < classesArray.Length; i++)
                 {
-                    Console.WriteLine(i + 1 + ") " + test[i].GetType().Name);
+                    Console.WriteLine(i + 1 + ") " + classesArray[i].GetType().Name);
                 }
-                Console.WriteLine("\nPlease pick method (numeric index)");
+                Console.WriteLine(k_PLEASE_PICK_AN_INDEX);
                 userInput = Console.ReadLine();
+                if (checkExitToken(userInput))
+                {
+                    Console.WriteLine(k_SAY_BYE);
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+
+                // if he didn't want to leave, we can go on
                 parseFlag = int.TryParse(userInput, out parsedVal);
                 if (parseFlag)
                 {
                     try
                     {
-                        Console.WriteLine(test[parsedVal - 1].GetType().Name);
-                        displayClass(test[parsedVal - 1].GetType().FullName);
+                        Console.WriteLine(classesArray[parsedVal - 1].GetType().Name);
+                        displayClass(classesArray[parsedVal - 1].GetType().FullName);
                     }
-                    catch (IndexOutOfRangeException e)
+                    catch (IndexOutOfRangeException)
                     {
-                        Console.WriteLine(e);
+                        Console.WriteLine(k_NO_SUCH_OPTION);
                     }
                 }
                 else
                 {
-                    Console.WriteLine("oops!");
+                    Console.WriteLine(k_WRONG_INPUT_TYPE);
                 }
+
+                // Make sure he read the info & pressed something
                 Console.ReadKey();
                 Console.Clear();
             }
-            Console.WriteLine("OOL");
+            Console.WriteLine(k_CONTACT_ADMIN);
             Console.ReadKey();
         }
 
         private void displayClass(string i_className)
         {
-            Type t = Type.GetType(i_className);
-            MethodInfo[] mi;
-            string userInput;
-            int parsedVal;
-            bool parsedFlag;
+            Type classType = Type.GetType(i_className);
+            MethodInfo[] methodsInfo;
+
+            // Instansiate to defaults
+            string userInput = "";
+            int parsedVal = -1;
+            bool parsedFlag = false;
+
             while (true)
             {
                 Console.Clear();
-                Console.WriteLine(t.Name);
-                Console.WriteLine("~~~~~~~~~~~~~~~");
-                mi = t.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
-                for (int i = 0; i < mi.Length; i++)
+                Console.WriteLine(classType.Name);
+                Console.WriteLine(k_HEADER_SEPARATOR);
+
+                // Get the methods info, filtered by only public, instance members & declared at this level (no inheritance)
+                methodsInfo = classType.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+                for (int i = 0; i < methodsInfo.Length; i++)
                 {
-                    Console.WriteLine((i + 1) + ") " + mi[i].Name);
+                    Console.WriteLine((i + 1) + ") " + methodsInfo[i].Name);
                 }
                 userInput = Console.ReadLine();
+                if (checkExitToken(userInput))
+                {
+                    Console.Clear();
+                    this.Show();
+                }
+
                 parsedFlag = int.TryParse(userInput, out parsedVal);
                 if (parsedFlag)
                 {
                     try
                     {
-                        object classInstance = Activator.CreateInstance(t, null);
-                        mi[parsedVal - 1].Invoke(classInstance, null);
+                        object classInstance = Activator.CreateInstance(classType, null);
+                        methodsInfo[parsedVal - 1].Invoke(classInstance, null);
                         Console.ReadKey();
-
                     }
                     catch (Exception)
                     {
-                        Console.WriteLine("We aint got that index dude...!");
+                        Console.WriteLine(k_NO_SUCH_OPTION);
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Make sure to enter a digit");
+                    Console.WriteLine(k_WRONG_INPUT_TYPE);
                 }
             }
+        }
 
-
-
-
-
-            Console.ReadKey();
+        private bool checkExitToken(string i_userInput)
+        {
+            return i_userInput.ToUpper() == k_EXIT_TOKEN;
         }
     }
 }
