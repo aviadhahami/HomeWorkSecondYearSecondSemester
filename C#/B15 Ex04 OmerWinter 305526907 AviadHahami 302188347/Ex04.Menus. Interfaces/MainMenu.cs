9f5cf61577c91d4bf2e5363ea5843d9a2a398c4a
Aviad Hahami <aviadhahami@gmail.com>
2015-06-04 22:12:39 +0300

    Done with Interfaces, fully implemented by required design

--- C#/B15 Ex04 OmerWinter 305526907 AviadHahami 302188347/Ex04.Menus. Interfaces/MainMenu.cs
+++ C#/B15 Ex04 OmerWinter 305526907 AviadHahami 302188347/Ex04.Menus. Interfaces/MainMenu.cs
@@ -7,13 +7,14 @@ namespace Ex04.Menus.Interfaces
 {
     public class MainMenu
     {
-        private const string k_WRONG_INPUT_TYPE = "Please type a proper choice index or 'exit' to leave";
+        private const string k_WRONG_INPUT_TYPE = "Please type a proper choice's index";
         private const string k_HEADER_SEPARATOR = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
-        private const string k_PLEASE_PICK_AN_INDEX = "Please pick a choice's index or 'exit' to leave";
+        private const string k_PLEASE_PICK_AN_INDEX = "Please pick a choice's index";
         private const string k_SAY_BYE = "Thank you and goodbye!";
         private const string k_NO_SUCH_OPTION = "There isn't such an option, retry";
         private const string k_CONTACT_ADMIN = "Something happened, please contant support";
-        private const string k_EXIT_TOKEN = "EXIT";
+        private const string k_EXIT_APP = "0) Exit application";
+        private const string k_GO_BACK = "0) Go back";
 
         // Classes members
         private InfoMenu m_InfoMenu;
@@ -39,23 +40,24 @@ namespace Ex04.Menus.Interfaces
             {
                 Console.WriteLine(currentMethod.GetType().Name);
                 Console.WriteLine(k_HEADER_SEPARATOR);
+                Console.WriteLine(k_EXIT_APP);
                 for (int i = 0; i < classesArray.Length; i++)
                 {
                     Console.WriteLine(i + 1 + ") " + classesArray[i].GetType().Name);
                 }
                 Console.WriteLine(k_PLEASE_PICK_AN_INDEX);
                 userInput = Console.ReadLine();
-                if (checkExitToken(userInput))
-                {
-                    Console.WriteLine(k_SAY_BYE);
-                    Console.ReadKey();
-                    Environment.Exit(0);
-                }
 
                 // if he didn't want to leave, we can go on
                 parseFlag = int.TryParse(userInput, out parsedVal);
                 if (parseFlag)
                 {
+                    if (parsedVal == 0)
+                    {
+                        Console.WriteLine(k_SAY_BYE);
+                        Console.ReadKey();
+                        Environment.Exit(0);
+                    }
                     try
                     {
                         Console.WriteLine(classesArray[parsedVal - 1].GetType().Name);
@@ -64,17 +66,21 @@ namespace Ex04.Menus.Interfaces
                     catch (IndexOutOfRangeException)
                     {
                         Console.WriteLine(k_NO_SUCH_OPTION);
+                        Console.ReadLine();
                     }
                 }
                 else
                 {
                     Console.WriteLine(k_WRONG_INPUT_TYPE);
+                    Console.ReadLine();
                 }
 
                 // Make sure he read the info & pressed something
-                Console.ReadKey();
+                // Console.Read();
                 Console.Clear();
             }
+
+            // Safe fallback
             Console.WriteLine(k_CONTACT_ADMIN);
             Console.ReadKey();
         }
@@ -94,28 +100,27 @@ namespace Ex04.Menus.Interfaces
                 Console.Clear();
                 Console.WriteLine(classType.Name);
                 Console.WriteLine(k_HEADER_SEPARATOR);
-
+                Console.WriteLine(k_GO_BACK);
                 // Get the methods info, filtered by only public, instance members & declared at this level (no inheritance)
                 methodsInfo = classType.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
                 for (int i = 0; i < methodsInfo.Length; i++)
                 {
                     Console.WriteLine((i + 1) + ") " + methodsInfo[i].Name);
                 }
+                Console.WriteLine(k_PLEASE_PICK_AN_INDEX);
                 userInput = Console.ReadLine();
-                if (checkExitToken(userInput))
-                {
-                    Console.Clear();
-                    this.Show();
-                }
-
                 parsedFlag = int.TryParse(userInput, out parsedVal);
                 if (parsedFlag)
                 {
+                    if (parsedVal == 0)
+                    {
+                        Console.Clear();
+                        break;
+                    }
                     try
                     {
                         object classInstance = Activator.CreateInstance(classType, null);
                         methodsInfo[parsedVal - 1].Invoke(classInstance, null);
-                        Console.ReadKey();
                     }
                     catch (Exception)
                     {
@@ -126,12 +131,8 @@ namespace Ex04.Menus.Interfaces
                 {
                     Console.WriteLine(k_WRONG_INPUT_TYPE);
                 }
+                Console.ReadKey();
             }
-        }
-
-        private bool checkExitToken(string i_userInput)
-        {
-            return i_userInput.ToUpper() == k_EXIT_TOKEN;
         }
     }
 }
