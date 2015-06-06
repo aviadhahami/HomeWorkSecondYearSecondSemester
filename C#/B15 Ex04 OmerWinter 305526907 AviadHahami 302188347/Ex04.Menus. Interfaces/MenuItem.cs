6a0d96eebbbe0a727809666d5ef1d8291eb44bc2
Aviad Hahami <aviadhahami@gmail.com>
2015-06-06 20:36:58 +0300

    done menu item

--- C#/B15 Ex04 OmerWinter 305526907 AviadHahami 302188347/Ex04.Menus. Interfaces/MenuItem.cs
+++ C#/B15 Ex04 OmerWinter 305526907 AviadHahami 302188347/Ex04.Menus. Interfaces/MenuItem.cs
@@ -6,5 +6,43 @@ namespace Ex04.Menus.Interfaces
 {
     class MenuItem
     {
+        private List<MenuItem> m_SubMenuItems;
+        private IPerformAction m_ActionToInvoke;
+        private string m_MenuTitle;
+
+        public MenuItem(string i_MenuTitle)
+        {
+            // Give it the proper name
+            m_MenuTitle = i_MenuTitle;
+
+            // Generate an empty list
+            m_SubMenuItems = new List<MenuItem>();
+        }
+
+        public MenuItem(string i_MenuTitle, IPerformAction i_ActionToInvoke)
+        {
+            m_MenuTitle = i_MenuTitle;
+            m_ActionToInvoke = i_ActionToInvoke;
+        }
+
+        public bool IsMenu()
+        {
+            return m_ActionToInvoke == null;
+        }
+
+        public List<MenuItem> GetMenuItems()
+        {
+            return m_SubMenuItems;
+        }
+
+        public void AddMenuItem(MenuItem i_MenuItem)
+        {
+            m_SubMenuItems.Add(i_MenuItem);
+        }
+
+        public void InvokeAction()
+        {
+            m_ActionToInvoke.performAction(m_MenuTitle);
+        }
     }
 }
